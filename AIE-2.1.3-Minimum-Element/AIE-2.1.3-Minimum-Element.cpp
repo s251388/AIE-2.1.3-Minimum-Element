6fbf7bd97a6e52d7cb2bb2de7e4ee949ddfe6598
s251388 <s251388@students.aie.edu.au>
2026-03-03 00:25:36 +1100

    Added int array and comments
    Added an array of ints and an assert that uses the function to check that the returned value of the array is 2.

--- AIE-2.1.3-Minimum-Element/AIE-2.1.3-Minimum-Element.cpp
+++ AIE-2.1.3-Minimum-Element/AIE-2.1.3-Minimum-Element.cpp
@@ -1,15 +1,16 @@
 
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
 
-int ArrayMin(int Array[], size_t Size) {
+int ArrayMin(int Array[], size_t Size) {			// Here i feed the size of the array in separately because it cannot be read from inside the function,
+													// i do this by dividing the total data size of the array by the data size of any one item in the array
+	int Min = Array[0];								// as all items in an array are the same amount of data.
 
-	int Min = Array[0];
-
-	for (int i = 0; i < Size; i++) {
-		if (Array[i] < Min) {
+	for (int i = 0; i < Size; i++) {				// This loop iterates through every item in the array and updates the Min int variable to a new value
+		if (Array[i] < Min) {						// whenever it is smaller than its previous value.
 			Min = Array[i];
 		}
 	}
@@ -20,6 +21,10 @@ int ArrayMin(int Array[], size_t Size) {
 
 int main()
 {
-    
+   
+	int IntArray[20] = {67, 13, 3, 89, 43, 2, 19, 71, 5, 61, 97, 7, 37, 31, 17, 11, 83, 53, 23, 29};
+
+	assert(ArrayMin(IntArray, sizeof(IntArray) / sizeof(IntArray[0])) == 2);
+
 }
 
